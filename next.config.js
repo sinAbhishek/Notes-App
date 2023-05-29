1b2235cccd2bd3be8b32d40982a6b7f69d4903fb
sinAbhishek <singhabhshek979@gmail.com>
2023-05-29 20:06:38 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: false,
+  },
+};
 
 module.exports = nextConfig;
 module.exports = {
